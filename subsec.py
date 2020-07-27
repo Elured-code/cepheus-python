7c57229950b41d865db37bb3da4cd4896ea90905
Michael Bailey <michaelk.t.bailey@gmail.com>
2020-07-27 13:24:32 +1000

    Comment fixes, more .gitignore fixes

--- subsec.py
+++ subsec.py
@@ -1,3 +1,27 @@
+#
+# Cepheus Engine SRD with Extensions Subsector Generation
+#
+# Version:  0.2
+# Author:   Michael Bailey
+# Date:     23 July 2020
+#
+
+#
+# This code generates an 8 x 10 subsector according to:
+# 1. CE SRD system generation rules; and
+# 2. Stellar data generation kludged together by myself with some quickly researched type/spectra
+#    distribution
+
+# 
+# Run subsec.py -h for usage
+#
+
+#
+# Things to do:
+#
+# 
+
+
 import argparse
 import random
 import TR_CE_EXT_World
